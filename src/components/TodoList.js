bc4ec20a008464787a38c3e0b95c6779758147dc
Taylor Crane <taylorkcrane@gmail.com>
2017-09-20 15:29:50 -0400

    minor formatting stuffs

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,50 +1,51 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 class TodoList extends React.Component {
-	constructor() {
-		super();
+    constructor() {
+        super();
 
-		this.handleAddTodo = this.handleAddTodo.bind(this);
-	}
+        this.handleAddTodo = this.handleAddTodo.bind(this);
+    }
 
-	handleAddTodo(event) {
-		event.preventDefault();
-		console.log(this.newTodo.value, this.newOwner.value);
-		if (this.newTodo.value.length > 0) {
-			this.props.addTodo(this.newTodo.value, this.newOwner.value);
-		}
-		this.newTodo.value = "";
-		this.newOwner.value = "";
-	}
-
-	render() {
-		return (
-			<div>
-				<ul>
-					{this.props.todos.map( (item, index) =>
-						<li key={index}>
-							<span>{item.todo}  --  </span>
-							<span>{item.owner ? `Owner: ${item.owner}  --  ` : null }</span>
-							<span className="complete" onClick={ () => this.props.completeTodo(index) }>
-								Done!
-							</span>
-						</li>
-					)}
-				</ul>
-				<form>
-					<input placeholder="Type a concern..." ref={newTodo => this.newTodo = newTodo} />
-					<input placeholder="Assign an owner..." ref={newOwner => this.newOwner = newOwner} />
-					<button onClick={this.handleAddTodo}>Add Concern</button>
-				</form>
-			</div>
-		)
-	}
+    handleAddTodo(event) {
+        event.preventDefault();
+        console.log(this.newTodo.value, this.newOwner.value);
+        if (this.newTodo.value.length > 0) {
+            this.props.addTodo(this.newTodo.value, this.newOwner.value);
+        }
+        this.newTodo.value = "";
+        this.newOwner.value = "";
+    }
+        
+    render() {
+        return (
+            <div>
+                <ul>
+                    {this.props.todos.map( (item, index) =>
+                        <li key={index}>
+                            <span>{item.todo}  --  </span>
+                            <span>{item.owner ? `Owner: ${item.owner}  --  ` : null }</span>
+                            <span className="complete" onClick={ () => this.props.completeTodo(index) }>
+                                Done!
+                            </span>
+                        </li>
+                    )}
+                </ul>
+                <form>
+                    <input placeholder="Type a concern..." ref={newTodo => this.newTodo = newTodo} />
+                    <input placeholder="Assign an owner..." ref={newOwner => this.newOwner = newOwner} />
+                    <button onClick={this.handleAddTodo}>Add Concern</button>
+                </form>
+            </div>
+        )
+    }
 }
 
 TodoList.propTypes = {
-	todos: React.PropTypes.array.isRequired,
-	addTodo: React.PropTypes.func.isRequired,
-	completeTodo: React.PropTypes.func.isRequired,
+    todos: PropTypes.array.isRequired,
+    addTodo: PropTypes.func.isRequired,
+    completeTodo: PropTypes.func.isRequired,
 }
 
 export default TodoList;
